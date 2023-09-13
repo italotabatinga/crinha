4f49b5bd2e9e3ed958d4982114a04c07c5ccf204
Italo Tabatinga <italotabatinga@gmail.com.br>
2023-09-13 02:22:33 -0300

    add math ops

--- src/vm.c
+++ src/vm.c
@@ -31,6 +31,12 @@ Value pop() {
 static InterpretResult run() {  // dispatching can be made faster with direct threaded code, jump table, computed goto
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do {                \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -49,6 +55,22 @@ static InterpretResult run() {  // dispatching can be made faster with direct th
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
         push(constant);
+        break;
+      }
+      case OP_ADD:
+        BINARY_OP(+);
+        break;
+      case OP_SUBTRACT:
+        BINARY_OP(-);
+        break;
+      case OP_MULTIPLY:
+        BINARY_OP(*);
+        break;
+      case OP_DIVIDE:
+        BINARY_OP(/);
+        break;
+      case OP_NEGATE: {
+        push(-pop());
         break;
       }
       case OP_RETURN:
