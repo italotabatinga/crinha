0774176db566d96f5629cee07204c4b6d26357fa
Italo Tabatinga <italotabatinga@gmail.com.br>
2023-09-13 02:08:57 -0300

    add vm stack

--- src/vm.c
+++ src/vm.c
@@ -7,10 +7,25 @@
 
 VM vm;  // if needed to run multiple vm, better if this is not global
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+  resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {  // dispatching can be made faster with direct threaded code, jump table, computed goto
@@ -19,6 +34,13 @@ static InterpretResult run() {  // dispatching can be made faster with direct th
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[");
+      printValue(*slot);
+      printf("]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -26,11 +48,12 @@ static InterpretResult run() {  // dispatching can be made faster with direct th
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
       case OP_RETURN:
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
     }
   }
