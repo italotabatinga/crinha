b1d9f59a9d50e6c167acc31ff8dfb0cf28f8d344
Italo Tabatinga <italotabatinga@gmail.com.br>
2023-09-18 14:55:04 -0300

    add OP_NOT

--- src/vm.c
+++ src/vm.c
@@ -47,6 +47,10 @@ static Value peek(int distance) {
   return vm.stackTop[-1 - distance];
 }
 
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {  // dispatching can be made faster with direct threaded code, jump table, computed goto
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -95,6 +99,7 @@ static InterpretResult run() {  // dispatching can be made faster with direct th
       case OP_DIVIDE:
         BINARY_OP(NUMBER_VAL, /);
         break;
+      case OP_NOT: push(BOOL_VAL(isFalsey(pop()))); break;
       case OP_NEGATE: {
         if (!IS_NUMBER(peek(0))) {
           runtimeError("Operand must be a number.");
