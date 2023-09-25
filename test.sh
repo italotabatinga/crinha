4a9269a27788c63dc02917c1824b34367ebbec9d
italotabatinga <italotabatinga@gmail.com.br>
2023-09-24 22:34:09 -0300

    fix test script

--- test.sh
+++ test.sh
@@ -3,36 +3,45 @@
 function setup() {
   echo -n "building..."
 
+  rm -rf tmp
   mkdir -p tmp
-  (make clean && make) |> /dev/null
+  make clean |> /dev/null
+  make |> /dev/null
   cp build/main tmp/crinha
   echo "done!"
   echo
 }
 
+function tests() {
+  e=0
+  for f in tests/*.rinha; do
+    filename=$(basename $f)
+    expected="$f.out"
+    result="tmp/$filename.out"
+
+    printf %-30s $filename | tr ' ' .
+
+    tmp/crinha $f > $result
+    if cmp -s $expected $result; then
+      echo OK
+    else
+      ((e+=1))
+      echo ERROR
+      echo "    expected: $(cat $expected)"
+      echo "    got:      $(cat $result)"
+    fi
+  done
+
+  return $e
+}
+
 setup
-e=0
-for f in tests/*.rinha; do
-  filename=$(basename $f)
-  expected="$f.out"
-  result="tmp/$filename.out"
+tests
 
-  printf %-30s $filename | tr ' ' .
-
-  tmp/crinha $f > $result
-  if cmp -s $expected $result; then
-    echo OK
-  else
-    ((e+=1))
-    echo ERROR
-    echo "    expected: $(cat $expected)"
-    echo "    got:      $(cat $result)"
-  fi
-done
-
-if (($e > 0)); then
+errors=$?
+if (($errors > 0)); then
   echo
-  echo "$e errors found"
+  echo "$errors errors found"
 fi
 
-exit $e
+exit $errors
